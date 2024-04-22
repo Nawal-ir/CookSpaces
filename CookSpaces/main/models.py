637b3e493bd9eef2160992cbcf9c6bb21522ffa4
Nawal <Nawalir.16@gmail.com>
2024-04-22 22:07:08 +0300

    Main App | adding Models, Views, Urls

--- CookSpaces/main/models.py
+++ CookSpaces/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator  
 
 # Create your models here.
+class Artical(models.Model):
+
+    title = models.CharField()
+    content = models.TextField()
+    poster = models.ImageField(upload_to='poster/')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Contact(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField()
+    email = models.EmailField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Review(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    evaluation = models.PositiveIntegerField( validators=[MinValueValidator(1), MaxValueValidator(5)], null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
--- CookSpaces/main/urls.py
+++ CookSpaces/main/urls.py
@@ -5,4 +5,11 @@ app_name = "main"
 
 urlpatterns  = [
     path("", views.home, name="home"),
+    path("All/", views.all_kitchen, name="all_kitchen"),
+    path("Detail/", views.kitchen_detail, name="kitchen_detail"),
+    path("Delete/", views.delete_kitchen, name="delete_kitchen"),
+    path("Articals/", views.articals, name="articals"),
+    path("About/", views.about, name="about"),
+    path("Contact/", views.contact, name="contact"),
+    path("reviews/add/", views.add_review, name="add_review"),
 ]
