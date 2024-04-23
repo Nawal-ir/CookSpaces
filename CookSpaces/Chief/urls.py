126247d59519ed9206c5b746428b4a2286d390d2
jmail <jamailotb15@gmail.com>
2024-04-23 14:16:27 +0300

    Chief App | delete chief detail template

--- CookSpaces/Chief/urls.py
+++ CookSpaces/Chief/urls.py
@@ -6,6 +6,5 @@ app_name = "Chief"
 urlpatterns  = [
     path('profile/', views.profile_view, name='profile'),
     path("all/", views.all_chief_view, name="all_chief_view"),
-    path('detail/', views.chife_detail, name='chife_detail')
 
 ]
