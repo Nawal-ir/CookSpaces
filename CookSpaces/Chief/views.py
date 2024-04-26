924262871ad18335b7adac04678dd1b43b1fc6d2
jmail <jamailotb15@gmail.com>
2024-04-26 18:19:06 +0300

    ..

--- CookSpaces/Chief/views.py
+++ CookSpaces/Chief/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,Group
 from django.contrib.auth import authenticate
 from django.db import IntegrityError,transaction
 from accounts.models import KitchenOwner,Renter,Chife
@@ -18,6 +18,12 @@ def register_chife(request:HttpRequest):
                 
                 new_user = User.objects.create_user(username=request.POST["username"], email=request.POST["email"], first_name=request.POST["first_name"], last_name=request.POST["last_name"], password=request.POST["password"])
                 new_user.save()
+
+                if not new_user.groups.filter(name='Chef').exists():
+                    group = Group.objects.get(name="Chef")
+                    new_user.groups.add(group)
+                
+                
 
                 
                 profile= Chife(user=new_user,about=request.POST["about"] ,avatar=request.FILES.get("avatar", KitchenOwner.avatar.field.get_default()),phone=request.POST["phone"], cv=request.FILES.get("cv"))
@@ -54,5 +60,10 @@ def all_chief_view(request: HttpRequest):
     chife = Chife.objects.all()
 
     return render(request,"chief/all_chief.html", {'chife': chife})
+
+
+
+def update_user_view(request: HttpRequest):
+    return render(request,"chief/update.html")
 
 
